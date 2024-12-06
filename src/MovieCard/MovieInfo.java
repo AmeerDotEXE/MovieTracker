956a15269ccff0a417cef049215fc59e292e88bf
AmeerDotEXE <ameerkhalid193@gmail.com>
2024-12-06 20:48:24 +0300

    updated movie info

--- src/MovieCard/MovieInfo.java
+++ src/MovieCard/MovieInfo.java
@@ -1,22 +1,40 @@
 package MovieCard;
 
 public class MovieInfo {
-	private String name;
-	private String duration;
+	private String name = "No Name";
+	private int durationMins;
 	private int year;
+	private boolean favorite = false;
+	private MovieStatus status = MovieStatus.Needs_Review;
+	private int rate;
+	private int lastWatched;
+	private String[] gerne = new String[4];
+	private String[] cast = new String[6];
+	private int firstWatched;
 
 	public MovieInfo(String name) {
 		this.name = name;
-		this.duration = "0h 0m";
-		this.year = 0;
 	}
-	public MovieInfo(String name, String duration, int year) {
+	public MovieInfo(String name, int durationMins, int year) {
 		this.name = name;
-		this.duration = duration;
+		this.durationMins = durationMins;
 		this.year = year;
 	}
 
 	public String getName() { return name; }
-	public String getDuration() { return duration; }
 	public int getYear() { return year; }
+	public int getDurationMins() { return durationMins; }
+	public boolean isFavorite() { return favorite; }
+	public MovieStatus getStatus() { return status; }
+	public int getRate() { return rate; }
+	public int getLastWatched() { return lastWatched; }
+	public String[] getGerne() { return gerne; }
+	public String[] getCast() { return cast; }
+	public int getFirstWatched() { return firstWatched; }
+
+	public String getDuration() {
+		int mins = durationMins % 60;
+		int hours = durationMins / 60;
+		return hours+"h "+mins+"m";
+	}
 }
---
+++ src/MovieCard/MovieStatus.java
@@ -0,0 +1,11 @@
+package MovieCard;
+
+public enum MovieStatus {
+	Needs_Review,
+	Might_Watch,
+	Want_to_Watch,
+	
+	Want_to_Rewatch,
+	
+	Watched,
+}
